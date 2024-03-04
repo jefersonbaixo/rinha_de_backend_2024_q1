3d2f1572943a9b846eece3ff2c39c3e7d5b745dc
jefersonbaixo <jeferson.baixo@gmail.com>
2024-03-04 15:00:34 -0300

    implemented too long description error.

--- lib/rinha_de_backend_2024_q1_web/controllers/fallback_controller.ex
+++ lib/rinha_de_backend_2024_q1_web/controllers/fallback_controller.ex
@@ -1,6 +1,13 @@
 defmodule RinhaDeBackend2024Q1Web.FallbackController do
   use RinhaDeBackend2024Q1Web, :controller
 
+  def call(conn, {:error, :string_data_right_truncation}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(json: RinhaDeBackend2024Q1Web.ErrorJSON)
+    |> render(:error, status: :unprocessable_entity)
+  end
+
   def call(conn, {:error, :not_found}) do
     conn
     |> put_status(:not_found)
